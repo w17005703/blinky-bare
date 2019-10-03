970d84cd08508146de80f7f8f75f63c930aec159
w17005703 <w17005703@northumbria.ac.uk>
2019-10-03 16:58:29 +0100

    Red leds

--- main.c
+++ main.c
@@ -13,6 +13,7 @@
  * See the reference manual, chapter 11, pp 277, and chapter 11, pp 283
  */ 
 #define PORTB_PCR21          (*((volatile uint32_t *)(0x4004A054u)))  //Ch11 pp277
+#define PORTB_PCR22	     (*((volatile uint32_t *)(0x4004A058u)))
 #define PORT_PCR_MUX_MASK    (0x0700u)   /*Ch11 pp283      */ 
 #define PORT_PCR_MUX_SHIFT   (8u)        /*ch11 pp283      */ 
 
@@ -23,7 +24,7 @@
 #define GPIOB_PDDR           (*((volatile uint32_t *)(0x400FF054u)))  /*ch55 pp1760 */
 #define GPIOB_PDOR           (*((volatile uint32_t *)(0x400FF040u)))  /*ch55 pp1759 */
 #define PIN21_MASK           (1u << 21)                               /*schematic pp3 RGB LED Feature PTB21 */
-
+#define PIN22_MASK           (1u << 22)
 void delay(int count);
 
 int main(void)
@@ -33,23 +34,30 @@ int main(void)
 
     /* Select the GPIO function (Alternative 1) for pin 21 of PORT B */
     PORTB_PCR21 &= ~PORT_PCR_MUX_MASK;           /*zero bits 8,9,10      */
-    PORTB_PCR21 |= (1u << PORT_PCR_MUX_SHIFT);   /*set them to 001 Alternative 1(GPIO)  */
+    PORTB_PCR21 |= (1u << PORT_PCR_MUX_SHIFT);
+    PORTB_PCR22 &= ~PORT_PCR_MUX_MASK;
+    PORTB_PCR22 |= (1u << PORT_PCR_MUX_SHIFT);   /*set them to 001 Alternative 1(GPIO)  */
 
     /* Set the data direction for pin 21 of PORT B to output */
     GPIOB_PDDR |= PIN21_MASK;		    /*or the value at *0x400FF054 with 0000 0000 0010 0000 0000 0000 0000 0000 */
+    GPIOB_PDDR |= PIN22_MASK;
+
     while (true) {
         /* Turn on the blue LED */
-        GPIOB_PDOR &= ~PIN21_MASK;		/*And the value in *0x400FF040 with 1111 1111 1101 1111 1111 1111 1111 1111
+        GPIOB_PDOR &= ~PIN21_MASK;
+	GPIOB_PDOR |= PIN22_MASK;		/*And the value in *0x400FF040 with 1111 1111 1101 1111 1111 1111 1111 1111
 		                                 setting to 0 sinks the current through the LED.*/
         /* Wait for about 1 second */
         delay(1000);
         /* Turn off the blue LED */
-        GPIOB_PDOR |= PIN21_MASK;       /*Or the value in *0x400FF040 with 0000 0000 0010 0000 0000 0000 0000 0000
+        GPIOB_PDOR |= PIN21_MASK; 
+	GPIOB_PDOR &= ~PIN22_MASK;      /*Or the value in *0x400FF040 with 0000 0000 0010 0000 0000 0000 0000 0000
 		                                seeting to 1 stops current flowing through the LED.*/   
         /* Wait for about 1 second */
         delay(1000);
     }
     return 0;
+
 }
 
 void delay(int count) {
@@ -58,3 +66,4 @@ void delay(int count) {
         /* skip */
     }
 }
+
